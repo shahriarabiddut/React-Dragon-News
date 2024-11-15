3f98d19e7ae914208a9e72bfe3f5d38db0040dc5
shahriarabiddut <shahriarhmed@gmail.com>
2024-11-15 18:49:22 +0600

    File Structure and Header

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header><Header/></header>
+            <nav>Nav</nav>
+            <main>Main</main>
+            <footer>Footer</footer>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/routes/router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import HomeLayout from '../layouts/HomeLayout';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <HomeLayout></HomeLayout>
+    },{
+        path:'/news',
+        element: <h1>News Layout</h1>
+    },{
+        path:'/auth',
+        element: <h1>Login Layout</h1>
+    },
+    {
+        path:'*',
+        element: <h1>Error Layout</h1>
+    },
+])
+
+export default router;
